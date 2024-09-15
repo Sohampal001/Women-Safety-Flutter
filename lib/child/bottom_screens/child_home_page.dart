01bb5bc52774ed2689a82cb25d796faa6d7ba351
Soham Pal <sohampal298@gmail.com>
2024-09-16 00:27:59 +0530

    Voice command sos alert implemented

--- lib/child/bottom_screens/child_home_page.dart
+++ lib/child/bottom_screens/child_home_page.dart
@@ -1,7 +1,15 @@
+import 'dart:async';
 import 'dart:math';
+import 'package:background_sms/background_sms.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:women_safety/db/db_services.dart';
+import 'package:women_safety/model/contactsm.dart';
 import 'package:women_safety/widgets/home_widgets/CustomCarouel.dart';
 import 'package:women_safety/widgets/home_widgets/custom_appBar.dart';
 import 'package:women_safety/widgets/home_widgets/emergency.dart';
@@ -20,6 +28,203 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _hasStarted = false; // Track if recognition has started once
   String _voiceStatus = "Not Listening";
   String _recognizedWords = "";
+  
+   Position? _currentPosition;
+  String? _currentAddress;
+  bool _isFetchingLocation = true;
+  int? _selectedSimSlot;
+  StreamSubscription<Position>? _positionStreamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _getPermission();
+    _startLocationTracking();
+    _loadSimSlot();
+    getRandomQuote();
+    _speech = stt.SpeechToText();
+    super.initState();
+    _initializeSpeechRecognition();
+  }
+
+  @override
+  void dispose() {
+    _positionStreamSubscription?.cancel();
+    super.dispose();
+  }
+
+  _getPermission() async {
+    var status = await Permission.sms.status;
+    if (status.isDenied || status.isPermanentlyDenied) {
+      await [Permission.sms, Permission.location].request();
+    }
+  }
+
+  _isPermissionGranted() async => await Permission.sms.status.isGranted;
+
+  _loadSimSlot() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _selectedSimSlot = prefs.getInt('simSlot');
+    });
+  }
+
+  _saveSimSlot(int simSlot) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('simSlot', simSlot);
+    setState(() {
+      _selectedSimSlot = simSlot;
+    });
+  }
+
+  Future<void> _sendSms(String phoneNumber, String message) async {
+    if (_selectedSimSlot == null) {
+      await _selectSimSlot();
+    }
+
+    if (_selectedSimSlot != null) {
+      var result = await BackgroundSms.sendMessage(
+        phoneNumber: phoneNumber,
+        message: message,
+        simSlot: _selectedSimSlot!,
+      );
+
+      if (result == SmsStatus.sent) {
+        Fluttertoast.showToast(msg: "Message sent through SIM ${_selectedSimSlot! + 1}");
+      } else {
+        Fluttertoast.showToast(msg: "Message failed to send");
+      }
+    }
+  }
+
+  Future<void> _selectSimSlot() async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Select SIM Slot"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text("SIM 1"),
+                onTap: () {
+                  _saveSimSlot(0);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text("SIM 2"),
+                onTap: () {
+                  _saveSimSlot(1);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _startLocationTracking() {
+    LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.medium,
+    );
+
+    AndroidSettings androidSettings = AndroidSettings(
+      accuracy: LocationAccuracy.medium,
+      distanceFilter: 10,
+    );
+
+    AppleSettings appleSettings = AppleSettings(
+      accuracy: LocationAccuracy.medium,
+      distanceFilter: 10,
+    );
+
+    _positionStreamSubscription = Geolocator.getPositionStream(
+      locationSettings: locationSettings,
+    ).listen((Position position) {
+      setState(() {
+        _currentPosition = position;
+        _fetchAddress();
+      });
+    });
+  }
+
+  Future<void> _fetchAddress() async {
+    if (_currentPosition == null) return;
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        _currentPosition!.latitude,
+        _currentPosition!.longitude,
+      );
+      if (!mounted) return;
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress = "${place.locality}, ${place.postalCode}, ${place.subLocality}";
+        _isFetchingLocation = false;
+      });
+    } catch (e) {
+      if (mounted) {
+        Fluttertoast.showToast(msg: "Failed to get address: $e");
+        setState(() {
+          _isFetchingLocation = false;
+        });
+      }
+    }
+  }
+
+  Future<void> _sendLocationToContacts() async {
+    if (_isFetchingLocation) {
+      Fluttertoast.showToast(msg: "Fetching location, please wait...");
+      return;
+    }
+
+    if (_currentPosition == null) {
+      Fluttertoast.showToast(msg: "Location is not available.");
+      return;
+    }
+
+    String messageBody = "https://www.google.com/maps/search/?api=1&query=${_currentPosition!.latitude}%2C${_currentPosition!.longitude}. $_currentAddress";
+
+    if (await _isPermissionGranted()) {
+      List<Tcontact> contactList = await DatabaseHelper().getContactList();
+      for (Tcontact contact in contactList) {
+        await _sendSms(contact.number, "I am in trouble, please reach me at $messageBody");
+      }
+    } else {
+      Fluttertoast.showToast(msg: "SMS permission not granted.");
+    }
+  }
+
+  void triggerSOS() async {
+    await _sendLocationToContacts();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   getRandomQuote() {
     Random random = Random();
@@ -28,13 +233,7 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  @override
-  void initState() {
-    getRandomQuote();
-    _speech = stt.SpeechToText();
-    super.initState();
-    _initializeSpeechRecognition();
-  }
+  
 
   void _initializeSpeechRecognition() async {
     bool available = await _speech.initialize(
@@ -79,8 +278,8 @@ class _HomeScreenState extends State<HomeScreen> {
         setState(() {
           _recognizedWords = val.recognizedWords;
 
-          if (_recognizedWords.toLowerCase().contains('hello')) {
-            _showToast("Hello Soham");
+          if (_recognizedWords.toLowerCase().contains('me to')) {
+            _sendLocationToContacts();
           }
         });
       },
