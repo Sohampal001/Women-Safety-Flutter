0b9df7fb2e90263554d551c6eb2cbcdc59ef46e6
Explore13 <suryaghoshchemistry2k21@gmail.com>
2024-09-27 02:01:56 +0530

    "Updated AndroidManifest.xml, added Google Maps API key, refactored SurroundedByMen widget to use Google Maps, and updated dependencies in pubspec.yaml."

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -6,13 +6,12 @@ class UserModel {
   String? parentEmail;
   String? type;
   String? gender;
-  
+
   double? latitude;
   double? longitude;
   String? photoUrl; // Add this line
 
   UserModel({
-<<<<<<< HEAD
     this.childEmail,
     this.name,
     this.parentEmail,
@@ -21,23 +20,20 @@ class UserModel {
     this.type,
     this.latitude,
     this.longitude,
+    this.gender,
     this.photoUrl, // Add this line
-=======
-    this.childEmail, this.name, this.parentEmail, this.phone,
-    this.id, this.type, this.latitude, this.longitude,this.gender
->>>>>>> 1c71e3f5019b210b4356e4268c53e2dcad7ee3dc
   });
 
   Map<String, dynamic> toJson() => {
-    'name': name,
-    'phone': phone,
-    'id': id,
-    'childEmail': childEmail,
-    'parentEmail': parentEmail,
-    'type': type,
-    'gender':gender,
-    'latitude': latitude,
-    'longitude': longitude,
-    'photoUrl': photoUrl, // Add this line
-  };
+        'name': name,
+        'phone': phone,
+        'id': id,
+        'childEmail': childEmail,
+        'parentEmail': parentEmail,
+        'type': type,
+        'gender': gender,
+        'latitude': latitude,
+        'longitude': longitude,
+        'photoUrl': photoUrl, // Add this line
+      };
 }
